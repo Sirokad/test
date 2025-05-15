2f7e04cb400d273ddb93de554b6778134a2bd426
Eyþór Óli Borgþórsson <eythor.borgthorsson@marel.com>
2025-05-15 13:22:10 +0000

    lala

--- home/urls.py
+++ home/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [ 
-    path('home', views.home),
-    path('authorized', views.authorized),
+urlpatterns = [
+    path('home/', views.HomeView.as_view(), name='home'),
+    path('authorized/', views.AuthorizedView.as_view(), name='authorized')
 ]
